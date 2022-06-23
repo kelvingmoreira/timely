6cf1b1af41e3632e44b032f2080e72786dd9aa6e
Kelvin Guedes <kelvin.guedes@tgcore.com.br>
2022-06-23 18:45:03 -0300

    cleaning usings.

--- src/Timely/Generics/TimelineEvent.cs
+++ src/Timely/Generics/TimelineEvent.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Timely.Generics;
-
 namespace Timely.Generics
 {
     public abstract class TimelineEvent<T> : ITimelineEvent<T> where T : class
